b8b1c45f5246af3e71fdad052e5140b0357613c2
zjwright <31255937+zjwright@users.noreply.github.com>
2017-10-19 18:33:18 -0500

    actual completed

--- Inclass14.m
+++ Inclass14.m
@@ -12,9 +12,30 @@ imshow(cat(3, stemcells_mask, im2double(imadjust(stemcells)), zeros(size(stemcel
 % (2) Try to separate touching objects using watershed. Use two different
 % ways to define the basins. 
 %(A) With erosion of the mask 
-stemcells_mask_erode=imerode(stemcells_mask, strel('disk',10));
-imshow(watershed(stemcells_mask)>1);
+figure(3)
+stemcells_mask_erode=imerode(stemcells_mask, strel('disk',8));
+%check if good erosion mask
+imshow(cat(3,stemcells_mask, stemcells_mask_erode, zeros(size(stemcells_mask))));
+figure(4)
+CC = bwconncomp(stemcells_mask);
+stemcells_stats = regionprops(CC, 'Area');
+stemcells_area = (stemcells_stats.Area);
+s = round(1.2*sqrt(mean(stemcells_area))/pi);
+stemcells_eroded = imerode(stemcells_mask, strel('disk',s));
+stemcells_outside= ~imdilate(stemcells_mask, strel('disk',1));
+stemcells_basin = imcomplement(bwdist(stemcells_outside));
+stemcells_basin = imimposemin(stemcells_basin,stemcells_eroded|stemcells_outside);
+stemcells_eroded_ws = watershed(stemcells_basin);
+rgb=label2rgb(stemcells_eroded_ws, 'jet', [.5 .5 .5]);
+imshow(rgb, 'InitialMagnification', 'fit')
+
 %(B) with a distance transform. 
-
-
+stemcells_D=bwdist(stemcells_mask);
+stemcells_D=-stemcells_D;
+stemcells_D(stemcells_mask)=-Inf;
+D_L=watershed(stemcells_D);
+rgb=label2rgb(D_L,'jet',[.5 .5 .5]);
+figure(5)
+imshow(rgb,'InitialMagnification','fit')
 %Which works better in this case?
+%the erosion worked better
